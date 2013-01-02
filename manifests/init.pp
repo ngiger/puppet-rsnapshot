591e9e6ad1ec95f08923cdf04eded17a94d8db28
Loic Dachary <loic@dachary.org>
2013-01-02 11:05:15 +0100

    write documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,6 +15,18 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+#
+# == Class: rsnapshot::puppetmaster
+#
+# Create the ssh key to be used by rsnapshot to backup a machine. It
+# must be part of the puppet master manifest definition.
+#
+# === Example
+#
+# node 'puppetmaster.domain.com' {
+#   class { 'rsnapshot::puppetmaster': }
+# }
+#
 class rsnapshot::puppetmaster {
   file {
     "/etc/puppet/modules/rsnapshot/files":
@@ -29,6 +41,19 @@ class rsnapshot::puppetmaster {
   }
 }
 
+#
+# == Class: rsnapshot::nagios
+#
+# Install a nagios plugin that checks the sanity of the rsnapshot
+# backups. It is designed to run on the machine hosting the backup and
+# should be called by nrpe or mrpe.
+#
+# === Example
+#
+# node 'nagios.domain.com' {
+#   class { 'rsnapshot::nagios': }
+# }
+#
 class rsnapshot::nagios {
   file {
     "/usr/lib/nagios":
@@ -44,6 +69,43 @@ class rsnapshot::nagios {
   }
 }
 
+#
+# == Class: rsnapshot::server
+#
+# Creates one rsnapshot configuration file for each machine for which
+# backup has been required by including rsnapshot::client in the
+# puppet manifest. A crontab is installed to run each configuration
+# file in sequence, daily, weekly and monthly. The logs are stored
+# in /var/log/rsnapshot.
+#
+# === Parameters
+#
+# [*ip*]
+#   A comma separated list of the ip addresses of the rsnapshot server.
+#   The machines to backup will use this list to restrict the incoming ssh
+#   connections (defaults to *$::ipaddress*).
+#
+# === Example
+#
+# node 'machineA.domain.com' {
+#   class { 'rsnapshot::client': }
+# }
+#
+# node 'machineB.domain.com' {
+#   class { 'rsnapshot::client': }
+# }
+#
+# node 'rsnapshot.domain.com' {
+#   class { 'rsnapshot::server': }
+# }
+#
+# Create one rsnapshot configuration file for *machineA.domain.com* and
+# *machineB.domain.com*.
+#
+# *machineA.domain.com* will accept rsync over ssh from rsnapshot.domain.com
+# default ip address ( *$::ipaddress* ).
+# 
+#
 class rsnapshot::server (
   $ip = $::ipaddress,
   ) {
@@ -92,6 +154,44 @@ class rsnapshot::server (
   }
 }
 
+#
+# == Class: rsnapshot::client
+#
+# Create a rsnapshot configuration file to be used by the machine running
+# rsnapshot. It is based on a template and exported to the puppet master.
+# It will install the ssh key of each rsnapshot server known to the puppet
+# master to give them the necessary access to perform the backups.
+#
+# === Parameters
+#
+# [*ip*]
+#   The ip address ( or hostname ) that the rsnapshot machine must use
+#   to get access to the machine. (defaults to *$::fqdn*).
+#
+# [*excludes*]
+#   An array of strings used to compose the *--exclude* arguments of
+#   the rsync command. (defaults to []).
+#
+# === Variables
+#
+# [*$::fqdn*]
+#   Is used as a name for the configuration file and within the
+#   configuration file itself to create unique files for logs etc.
+#
+# === Example
+#
+# node 'debian.novalocal' {
+#  class { 'rsnapshot::client':
+#    ip => $::ipaddress,
+#    excludes => [ '/media/debian', '/backup' ],
+# }
+#
+# Because the *$::fqdn* of *debian.novalocal* does not resolve in the context
+# of the machine running rsnapshot, it is replaced by the ip address of the
+# machine. The */media/debian* and */backup* directory on *debian.novalocal*
+# must not be included in the backup. The *excludes* argument will translate
+# into *--exlude=/media/debian* *--exclude=/backup* on the rsync command line.
+# 
 class rsnapshot::client (
   $excludes = [],
   $ip = $::fqdn,
